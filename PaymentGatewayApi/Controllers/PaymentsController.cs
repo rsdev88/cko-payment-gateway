9a5f5e8f6db31d00d9a2b4e587c33699d99c4652
Rob Sangar <robsangar@gmail.com>
2020-09-17 12:38:29 +0100

    App initialisation (including .gitignore file)

---
+++ PaymentGatewayApi/Controllers/PaymentsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentGatewayApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello world!");
+        }
+    }
+}
